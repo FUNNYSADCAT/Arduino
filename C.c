f02e96d18c68be24dc7282f891ec2f4b9f3a8a67
FUNNYSADCAT <thanap198@gmail.com>
2026-08-14 14:47:50 +0700

    Refactor motor control using Motor class

--- C.c
+++ C.c
@@ -1,40 +1,42 @@
+class Motor {
+  int pinA, pinB;
+
+public:
+  Motor(int a, int b) : pinA(a), pinB(b) {}
+
+  void begin() {
+    ledcAttach(pinA, 5000, 8);
+    ledcAttach(pinB, 5000, 8);
+  }
+
+  void setSpeed(int speed) {
+    speed = constrain(speed, -255, 255);
+    if (speed >= 0) {
+      ledcWrite(pinA, speed);
+      ledcWrite(pinB, 0);
+    } else {
+      ledcWrite(pinA, 0);
+      ledcWrite(pinB, -speed);
+    }
+  }
+};
+
 const int sensorPins[5] = {17, 16, 26, 25, 33};
 
-const int MOTOR_L_A = 13;
-const int MOTOR_L_B = 12;
-const int MOTOR_R_A = 27;
-const int MOTOR_R_B = 14;
+Motor motorL(13, 12);
+Motor motorR(27, 14);
 
 const int THRESHOLD = 2000;
 const int BASE_SPEED = 150;
 const int TURN_SPEED = 80;
-const int MAX_SPEED = 255;
 
 int lastTurn = 0;
-
-void driveOneMotor(int pinA, int pinB, int speed) {
-  speed = constrain(speed, -MAX_SPEED, MAX_SPEED);
-  if (speed >= 0) {
-    ledcWrite(pinA, speed);
-    ledcWrite(pinB, 0);
-  } else {
-    ledcWrite(pinA, 0);
-    ledcWrite(pinB, -speed);
-  }
-}
-
-void setMotorSpeed(int left, int right) {
-  driveOneMotor(MOTOR_L_A, MOTOR_L_B, left);
-  driveOneMotor(MOTOR_R_A, MOTOR_R_B, right);
-}
 
 void setup() {
   Serial.begin(115200);
   for (int i = 0; i < 5; i++) pinMode(sensorPins[i], INPUT);
-  ledcAttach(MOTOR_L_A, 5000, 8);
-  ledcAttach(MOTOR_L_B, 5000, 8);
-  ledcAttach(MOTOR_R_A, 5000, 8);
-  ledcAttach(MOTOR_R_B, 5000, 8);
+  motorL.begin();
+  motorR.begin();
 }
 
 void loop() {
@@ -45,26 +47,34 @@ void loop() {
   bool s4 = analogRead(sensorPins[4]) > THRESHOLD;
 
   if (s2) {
-    setMotorSpeed(BASE_SPEED, BASE_SPEED);
+    motorL.setSpeed(BASE_SPEED);
+    motorR.setSpeed(BASE_SPEED);
     lastTurn = 0;
   } else if (s1) {
-    setMotorSpeed(BASE_SPEED - TURN_SPEED, BASE_SPEED);
+    motorL.setSpeed(BASE_SPEED - TURN_SPEED);
+    motorR.setSpeed(BASE_SPEED);
     lastTurn = -1;
   } else if (s0) {
-    setMotorSpeed(0, BASE_SPEED);
+    motorL.setSpeed(0);
+    motorR.setSpeed(BASE_SPEED);
     lastTurn = -1;
   } else if (s3) {
-    setMotorSpeed(BASE_SPEED, BASE_SPEED - TURN_SPEED);
+    motorL.setSpeed(BASE_SPEED);
+    motorR.setSpeed(BASE_SPEED - TURN_SPEED);
     lastTurn = 1;
   } else if (s4) {
-    setMotorSpeed(BASE_SPEED, 0);
+    motorL.setSpeed(BASE_SPEED);
+    motorR.setSpeed(0);
     lastTurn = 1;
   } else if (lastTurn < 0) {
-    setMotorSpeed(-BASE_SPEED, BASE_SPEED);
+    motorL.setSpeed(-BASE_SPEED);
+    motorR.setSpeed(BASE_SPEED);
   } else if (lastTurn > 0) {
-    setMotorSpeed(BASE_SPEED, -BASE_SPEED);
+    motorL.setSpeed(BASE_SPEED);
+    motorR.setSpeed(-BASE_SPEED);
   } else {
-    setMotorSpeed(BASE_SPEED, BASE_SPEED);
+    motorL.setSpeed(BASE_SPEED);
+    motorR.setSpeed(BASE_SPEED);
   }
 
   Serial.printf("s=%d%d%d%d%d lastTurn=%d\n", s0, s1, s2, s3, s4, lastTurn);
